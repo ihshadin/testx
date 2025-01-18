d19b591d9e8a3a47dc5399440e0dc688013a9e7a
jahid <jahidmorol2@gmail.com>
2025-01-18 22:00:44 +0600

    update user status

--- src/app/api/v1/user/[id]/route.ts
+++ src/app/api/v1/user/[id]/route.ts
@@ -5,6 +5,7 @@ import { ApiError } from "next/dist/server/api-utils";
 import { UserModel } from "../userModule/user.model";
 import sendApiResponse from "@/utils/Response/sendResponse";
 import { handleError } from "@/utils/errors/handleError";
+import { userUpdateValidationSchema } from "../userModule/user.validation";
 
 export async function GET(
   req: NextRequest,
@@ -27,6 +28,51 @@ export async function GET(
       statusCode: 200,
       success: true,
       message: "Retrieved a User",
+      data: result,
+    });
+  } catch (error: any) {
+    return handleError(error, NextResponse);
+  }
+}
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const id = params.id;
+    const adminVerify = verifyAdmin(req);
+
+    if (!adminVerify) {
+      throw new ApiError(401, "You are Unauthorized");
+    }
+
+    await dbConnect();
+
+    const isExist = await UserModel.findOne({
+      _id: id,
+    }).select("_id");
+
+    if (!isExist) {
+      throw new ApiError(404, "User is not found!");
+    }
+
+    const payload = await req.json();
+
+    console.log("payload--=>", payload);
+
+    const validateData = userUpdateValidationSchema.parse(payload);
+
+    console.log("validateData", validateData);
+
+    const result = await UserModel.findByIdAndUpdate(id, validateData, {
+      new: true,
+    });
+
+    return sendApiResponse(NextResponse, {
+      statusCode: 200,
+      success: true,
+      message: "User Updated Successfully!",
       data: result,
     });
   } catch (error: any) {
